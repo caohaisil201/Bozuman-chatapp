eb49fba2a9753210cb249b1d3c240625ff3044ed
caohaisil201 <caohaisil@gmail.com>
2022-08-01 10:46:54 +0700

    [hotfix/authentication] delete logic in _app, move it to ProtectedRoute component

---
+++ front-end/components/ProtectedRoute/index.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import axiosClient from 'helper/axiosClient';
+import { appRoutes } from '../../constant/appRoutes';
+//TODO: fix type of Props
+interface Props {
+  router: any,
+  children: any,
+}
+
+const ProtectedRoute = ({router, children}: Props) =>{
+  const unProtectedRoutes = [
+    appRoutes.SIGN_IN_PAGE,
+    appRoutes.SIGN_UP_PAGE,
+    appRoutes.FORGOT_PASSWORD_PAGE,
+    appRoutes.RESET_PASSWORD_PAGE,
+    appRoutes.SIGN_UP_SUCCESS_PAGE,
+    appRoutes.ACTIVATE_ACCOUNT_PAGE,
+  ]
+  
+  async function checkLogin() {
+    const res: any = await axiosClient.get(`${process.env.NEXT_PUBLIC_DOMAIN}/`).then(res => res);
+    if(res.response){
+      return false;
+    }
+    return true;
+  }
+
+  async function checkAuth() {
+    const pathIsProtected = unProtectedRoutes.indexOf(router.pathname) === -1;
+    const isLogin: boolean = await checkLogin();
+    if(pathIsProtected && !isLogin) {
+      router.push('/sign-in');
+    }else if(!pathIsProtected && isLogin){
+      router.push('/');
+    }
+  }
+
+  useEffect(()=>{
+    checkAuth();
+  },[])
+
+  return children;
+}
+
+export default ProtectedRoute;
